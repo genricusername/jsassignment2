6aeb089733650d6a09e7580d0fd86ee59fefbcd1
Cody McB <codymb2004@outlook.com>
2025-07-09 20:16:32 -0400

    added id's and values to the base html and made a function to get the select option before passing it onto the constructor

--- js.js
+++ js.js
@@ -1,9 +1,23 @@
-function Coffee(size, liquid, baseCombo, extras) {
+
+//find the button by the submit id and when clicked call submitorder
+document.getElementById("submit").addEventListener("click", submitOrder)
+
+
+function submitOrder(){
+    //from the drop down menus get what they selected and pass it down to the constructor
+    let sizeGet = document.getElementById("sizeSelect").value;
+    let liquidGet = document.getElementById("liquidSelect").value;
+    let baseComboGet = document.getElementById("baseSelect").value;
+    let extrasGet = document.getElementById("extras").value;
+
+    Smoothie(sizeGet, liquidGet, baseComboGet, extrasGet);
+}
+
+
+function Smoothie(size, liquid, baseCombo, extras) {
     this.size = size;
     this.liquid = liquid;
     this.baseCombo = BaseCombo;
     this.extras = extras;
-    this.description = function () {
-        return `A ${this.size} sized ${this.liquid} and ${this.baseCombo} smoothie with ${this.extras} to top it off.`;
-    };
+
 }
